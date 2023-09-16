c64faa882f6be046e7b47ffefbc6be28b9830d8a
Fu Zi Xiang <speed2exe@live.com.sg>
2023-09-17 00:03:10 +0800

    chore: add test to main

--- src/main.zig
+++ src/main.zig
@@ -8,3 +8,6 @@ export fn add(a: i32, b: i32) i32 {
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
 }
+
+// The followings are the actual tests
+const _ = @import("./mysql.zig");
