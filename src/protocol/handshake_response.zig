b638072e7c8c38a008d1c36a51ad9986d983904a
Fu Zi Xiang <speed2exe@live.com.sg>
2023-10-07 12:00:04 +0800

    feat: done with handshake response 41

--- src/protocol/handshake_response.zig
+++ src/protocol/handshake_response.zig
@@ -9,7 +9,7 @@ pub const HandshakeResponse320 = struct {
     max_packet_size: u24,
     username: [:0]const u8,
     auth_response: [:0]const u8,
-    database: ?[:0]const u8,
+    database: [:0]const u8 = .{},
 
     pub fn write(h: HandshakeResponse320, writer: anytype, capabilities: u32) !void {
         try packer_writer.writeUInt16(writer, h.client_flags);
@@ -28,21 +28,18 @@ pub const HandshakeResponse41 = struct {
     client_flags: u32,
     max_packet_size: u32,
     character_set: u8,
-    filler: [23]u8 = .{0} ** 23,
     username: [:0]const u8,
-    auth_response_length: ?u8,
     auth_response: []const u8,
-    database: ?[:0]const u8,
-    client_plugin_name: ?[:0]const u8,
-    length_of_all_key_values: ?u64,
-    key_values: ?[]const [2][]const u8,
-    zstd_compression_level: ?u8,
+    database: [:0]const u8 = .{},
+    client_plugin_name: [:0]const u8 = .{},
+    key_values: []const [2][]const u8 = .{},
+    zstd_compression_level: u8 = 0,
 
     pub fn write(h: HandshakeResponse41, writer: anytype, capabilities: u32) !void {
         try packer_writer.writeUInt32(writer, h.client_flags);
         try packer_writer.writeUInt24(writer, h.max_packet_size);
         try packer_writer.writeUInt8(writer, h.character_set);
-        _ = try writer.write(h.filler);
+        _ = try writer.write(&[0]u8 ** 23); // filler
         try packer_writer.writeNullTerminatedString(writer, h.username);
 
         if ((capabilities & constants.CLIENT_PLUGIN_AUTH_LENENC_CLIENT_DATA) > 0) {
@@ -51,7 +48,6 @@ pub const HandshakeResponse41 = struct {
             try packer_writer.writeUInt8(writer, h.auth_response_length);
             _ = try writer.write(h.auth_response);
         }
-
         if ((capabilities & constants.CLIENT_CONNECT_WITH_DB) > 0) {
             try packer_writer.writeNullTerminatedString(writer, h.database);
         }
@@ -59,14 +55,14 @@ pub const HandshakeResponse41 = struct {
             try packer_writer.writeNullTerminatedString(writer, h.client_plugin_name);
         }
         if ((capabilities & constants.CLIENT_CONNECT_ATTRS) > 0) {
-            if (h.key_values) |key_values| {
-                try packer_writer.writeLengthEncodedInteger(writer, key_values.len);
-                @panic("pause here");
-            } else {
-                try packer_writer.writeLengthEncodedInteger(writer, 0);
+            try packer_writer.writeLengthEncodedInteger(writer, h.key_values.len);
+            for (h.key_values) |key_value| {
+                try packer_writer.writeLengthEncodedString(writer, key_value[0]);
+                try packer_writer.writeLengthEncodedString(writer, key_value[1]);
             }
         }
-
-        @panic("not done");
+        if ((capabilities & constants.CLIENT_ZSTD_COMPRESSION_ALGORITHM) > 0) {
+            try packer_writer.writeUInt8(writer, h.zstd_compression_level);
+        }
     }
 };
